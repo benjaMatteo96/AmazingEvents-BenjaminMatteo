71844cd7697e9bed763dd4bfd9d9e2d87919c295
benjaMatteo96 <benjaminmatteo.96@gmail.com>
2023-08-15 22:52:06 -0400

    update upcoming

--- assets/scripts/upcomingEvents.js
+++ assets/scripts/upcomingEvents.js
@@ -1,11 +1,48 @@
-const UpComingCards= document.getElementById('UpComingCards')
+const UpComingCards = document.getElementById('UpComingCards')
 const checkBoxes = document.getElementById("checkBoxes-container")
 const inputBusqueda = document.getElementById("input-busqueda")
 const buttonBusqueda = document.getElementById("button-busqueda")
 const formCheckAndBusqueda = document.getElementById("form")
 /* Seleccion los elementos de html */
 
-function crearTarjeta (data){
+fetch("https://mindhub-xj03.onrender.com/api/amazing")
+  .then(response => response.json())
+  .then(response => {
+    const newEventsobject = response;
+    const currentDate = response.currentDate
+    console.log(currentDate)
+    const events = newEventsobject.events
+    console.log(events)
+   /*  const objectNames = events.map(objects => objects.name)
+    console.log(objectNames) */
+    mostrarTarjeta(events, UpComingCards)
+    imprimirCheckboxes(events, checkBoxes)
+    filtrarDatos(events,currentDate)
+    /* filtroPorBusqueda(events,inputBusqueda.value)
+    filtroPorCheck(events) */
+
+    buttonBusqueda.addEventListener("click", (e) => {
+      e.preventDefault()
+      const valorIngresado = inputBusqueda.value;
+      console.log(typeof valorIngresado);
+      let filtro = filtroPorBusqueda( valorIngresado, events)
+      let filtroCruzado = filtroPorCheck(filtro)
+      mostrarTarjeta(filtroCruzado, UpComingCards)
+
+    });
+
+    checkBoxes.addEventListener('change', () => {
+
+      const valorIngresado = inputBusqueda.value;
+      console.log(valorIngresado)
+      let filtro = filtroPorBusqueda(valorIngresado, events)
+      let filtroCruzado = filtroPorCheck(filtro)
+      mostrarTarjeta(filtroCruzado, UpComingCards)
+    })
+  })
+
+
+function crearTarjeta(data) {
 
   return `<div class="card" style="width: 18rem;">
           <img src = "${data.image}" class="card-img-top" alt="Imagen">
@@ -20,115 +57,80 @@ function crearTarjeta (data){
         </div>`
 }
 
-function filtrarDatos(datos){
+function filtrarDatos(datos,currentDate) {
   const datosFiltrados = [] /*Aqui van los elementos asociados a la condicion de abajo. */
-  for (const dato of datos ) {
-    if (dato.date > data.currentDate){
+  for (const dato of datos) {
+    if (dato.date > currentDate) {
       datosFiltrados.push(dato)
     }
   }
-  
+
   return datosFiltrados
 }
 
 
-const UpComingEvents = filtrarDatos(data.events)
+// const UpComingEvents = filtrarDatos(data.events)
 
-function mostrarTarjeta (listaDeDatos, card) {
+function mostrarTarjeta(listaDeDatos, card) {
   card.innerHTML = " " /* Borro el contenido de card */
-  for ( datas of listaDeDatos) { 
-        const crear = crearTarjeta (datas)/* Manipular cada elemento de la lista UpcomingEvents en la plantillas sting  dado en crear tarjetas */
-        card.innerHTML += crear /* Mostrar dicho string previamente manipulado  */
+  for (datas of listaDeDatos) {
+    const crear = crearTarjeta(datas)/* Manipular cada elemento de la lista UpcomingEvents en la plantillas sting  dado en crear tarjetas */
+    card.innerHTML += crear /* Mostrar dicho string previamente manipulado  */
   }
 }
-mostrarTarjeta(UpComingEvents, UpComingCards)  
-
-
-function crearCheckboxes(elemento){
+function crearCheckboxes(elemento) {
   return `<div>
            <input type="checkbox" id="${elemento}">
            <label for="${elemento}">${elemento}</label>
          </div>`
- }
-
-/* Creo una lista, que contenga como elementos, las categorias */
-const categoriaDeEventos = UpComingEvents.map(categoria => categoria.category)
-
-/* Hago que en mi lista de categorias no se repita ningun elemento */
-/* Set es una estructura de datos */
-const set = new Set (categoriaDeEventos)
-
-/* Creo una nueva array con los elementos de mi constante set */
-const categoriasSinRepetir = Array.from(set)
-
-  function imprimirCheckboxes(listaEventos, checkboxes){
-   
-    for (const elemento of listaEventos) {
-      
-        const checkbox = crearCheckboxes(elemento)
-        checkboxes.innerHTML+=checkbox  
-      }
-      
-    } 
-
-imprimirCheckboxes(categoriasSinRepetir, checkBoxes)
-
-
-
-function filtroPorCheck (listaEventos) {
-
-  /* todos los inputs de tipo checkbox con la propiedad checked */
-  const allCheckboxes= document.querySelectorAll("input[type='checkbox']:checked")
-  console.log(allCheckboxes)
-  /* Como lo anterior me devuelve un node list, para trabajarlo, lo converti en un array */
-  const arrayChecks = Array.from(allCheckboxes)
-  /* De este nuevo array solo quiero trabajar con el id */
-  const arrayChecksMap = arrayChecks.map(elemento => elemento.id.toLowerCase())
-  console.log(arrayChecksMap)
-  /* De mi array eventos, filtro solo los objetos (evento) donde el valor de categoria es igual al valor de mi arrayCheckMaps */
-  const filtrarEventos =  listaEventos.filter(evento => arrayChecksMap.includes(evento.category.toLowerCase()) || arrayChecksMap.length === 0)
-  console.log(filtrarEventos)
-  return filtrarEventos
-  
 }
-/* filtroPorCheck(checkBoxes) /* Termina filtro por checkbox */ 
+
+
+function imprimirCheckboxes(listaEventos, checkboxes) {
+  const categoriaDeEventos = listaEventos.map(categoria => categoria.category)
+  const set = new Set(categoriaDeEventos)
+  const categoriasSinRepetir = Array.from(set)
+  console.log(categoriasSinRepetir)
+  for (const elemento of categoriasSinRepetir ) {
+
+    const checkbox = crearCheckboxes(elemento)
+    checkboxes.innerHTML += checkbox
+  }
+return categoriasSinRepetir
+}
+
+/* imprimirCheckboxes(categoriasSinRepetir, checkBoxes) */
+
+
+
+function filtroPorCheck(listaEventos) {
+
+  const allCheckboxes = document.querySelectorAll("input[type='checkbox']:checked")
+  const arrayChecks = Array.from(allCheckboxes)
+  const arrayChecksMap = arrayChecks.map(elemento => elemento.id.toLowerCase())
+  const filtrarEventos = listaEventos.filter(evento => arrayChecksMap.includes(evento.category.toLowerCase()) || arrayChecksMap.length === 0)
+
+  return filtrarEventos
+
+}
+/* filtroPorCheck(checkBoxes) /* Termina filtro por checkbox */
 
 /* <--------------------------------------------------------------------------------------> */
 
 function filtroPorBusqueda(valor, data) {
-
+  console.log(data)
   if (valor === "") {
     return data
   }
-  /*El filtro tiene los elementos de mi array UpComingEvents que incluyan el valor ingresado por el usuario (variable llamada valor Ingresado.) */
   const filtrobusqueda = data.filter(objeto => objeto.name.toLowerCase().includes(valor.toLowerCase()))
-  console.log(filtrobusqueda) 
+  console.log(filtrobusqueda)
   return filtrobusqueda
 
 }
 
-buttonBusqueda.addEventListener("click", (e) => {
-e.preventDefault()
-const valorIngresado = inputBusqueda.value;
-/* console.log(valorIngresado); */
-let filtro = filtroPorBusqueda(valorIngresado,UpComingEvents)
-/* filtroCruzado  almacena la funcion filtroPorCheck usando como argumento el filtro por busqueda*/
-let filtroCruzado = filtroPorCheck(filtro)
-mostrarTarjeta(filtroCruzado,UpComingCards)
- 
-
-});
-
-checkBoxes.addEventListener('change', () => {
-const valorIngresado = inputBusqueda.value;
-let filtro = filtroPorBusqueda(valorIngresado,UpComingEvents)
-let filtroCruzado = filtroPorCheck(filtro)
-mostrarTarjeta(filtroCruzado,UpComingCards)
-})
-
-function filtroCruzado(valor,valor2){
+/* function filtroCruzado(valor,valor2){
 filtroPorBusqueda(valor)
 filtroPorCheck(valor2)
 }
-
+ */
 /* filtroCruzado() */
